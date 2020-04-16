3b1f5dfda194a55a80a8724452828a9dc6f4389a
Jonathan Moore <programjm@gmail.com>
2020-04-15 20:06:42 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/bathroomproblem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bathroomproblem.dir/main.cpp.o"
+  "bathroomproblem.pdb"
+  "bathroomproblem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bathroomproblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/bathroomproblem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bathroomproblem.
+# This may be replaced when dependencies are built.
