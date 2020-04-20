9cb9bbe182a69db0e78ce8c94118321b5e7c053e
Jonathan Moore <programjm@gmail.com>
2020-04-19 21:33:16 -0500

    Add comments

--- src/MyBathroomProblemSolver.java
+++ src/MyBathroomProblemSolver.java
@@ -2,16 +2,16 @@ import java.util.concurrent.Semaphore;
 
 public class MyBathroomProblemSolver implements BathroomProblemSolverInterface
 {
-	Semaphore toilets;
-    Semaphore mutex = new Semaphore(1);
-    int n;
-    int[] inBathroom = {0,0};
+	Semaphore toilets; // The number of available toilets
+    Semaphore mutex = new Semaphore(1); // Mutex for mutual exclusion in critical section
+    int totalToilets; // total number of toilets (free and occupied)
+    int[] inBathroom = {0,0}; // number of men and women in bathroom (should only be one gender at a time)
 
     public MyBathroomProblemSolver(int n)
 	{
 	    System.out.println("MyBathroomProblemSolver("+n+")");
-		toilets = new Semaphore(n);
-		this.n=n;
+		toilets = new Semaphore(n); // n toilets free at start
+		this.totalToilets = n; // n toilets total
 	}
 
 
@@ -25,16 +25,16 @@ public class MyBathroomProblemSolver implements BathroomProblemSolverInterface
 
 	    boolean done = false;
 	    while (!done) {
-	        toilets.acquireUninterruptibly();
-	        mutex.acquireUninterruptibly();
-	        if (inBathroom[gender.opposite(type).ordinal()] == 0
-            && inBathroom[type.ordinal()] < n) {
+	        toilets.acquireUninterruptibly(); // wait for a toilet
+	        mutex.acquireUninterruptibly(); // enter CS
+	        if (inBathroom[gender.opposite(type).ordinal()] == 0 // if none of other gender inside, and <N of same gender inside, then enter
+            && inBathroom[type.ordinal()] < totalToilets) {
                 inBathroom[type.ordinal()]++;
                 done = true;
             }
 	        else
-	            toilets.release();
-            mutex.release();
+	            toilets.release(); // else, wait for another opportunity & signal someone else to try
+            mutex.release(); // leave CS
         }
 
         System.out.println("END enterTheBathroom"+type+")");
@@ -47,10 +47,10 @@ public class MyBathroomProblemSolver implements BathroomProblemSolverInterface
 	    System.out.println("leaveTheBathroom"+type+")");
 	    debug();
 
-	    mutex.acquireUninterruptibly();
-	    inBathroom[type.ordinal()]--;
-	    mutex.release();
-        toilets.release();
+	    mutex.acquireUninterruptibly(); // enter CS
+	    inBathroom[type.ordinal()]--; // leave bathroom
+	    mutex.release(); // leave CS
+        toilets.release(); // signal a person waiting for a toilet
         System.out.println("END leaveTheBathroom"+type+")");
         debug();
 	}
